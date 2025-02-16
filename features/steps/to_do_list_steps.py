93b4f5f9bd4762d7b480501ca325db4f110ea4ea
Bohdan Remeniuk <bogdansnou@gmail.com>
2025-02-16 13:52:45 +0200

    firs test update

--- features/steps/to_do_list_steps.py
+++ features/steps/to_do_list_steps.py
@@ -1,8 +1,8 @@
 from behave import given, when, then
 from to_do_list import Task, ToDoList
 
-@given('I want to create a task with the following details')
-def get_data(context):
+@given('I want to create a tasks with the following details')
+def prepare_tasks_data(context):
     context.task_data = {}
     for row in context.table:
         context.task_data[row['title']] = {
@@ -13,33 +13,56 @@ def get_data(context):
 
     context.todo_list = ToDoList()
 
-@when('I add the task "{task_title}" to the list')
-def add_task_to_the_list(context, task_title):
+@when('I add the tasks "{task1}", "{task2}" and "{task3}" to the list')
+def add_multiple_tasks(context, task1, task2, task3):
     try:
-        task_info = context.task_data[task_title]
-        context.todo_list.add_task(
-            task_title=task_title,
-            task_text=task_info['description'],
-            deadline=task_info['deadline'],
-            priority=task_info['priority']
-        )
-        context.task_added = True
+        for task_title in [task1, task2, task3]:
+            task_info = context.task_data[task_title]
+            context.todo_list.add_task(
+                task_title=task_title,
+                task_text=task_info['description'],
+                deadline=task_info['deadline'],
+                priority=task_info['priority']
+            )
+        context.tasks_added = True
     except Exception as e:
-        context.task_added = False
+        context.tasks_added = False
         context.error = str(e)
 
-@then('the task should be successfully created')
-def step_impl(context):
-    assert context.task_added, "Task was not created successfully"
+@then('the tasks should be successfully created')
+def verify_tasks_creation(context):
+    assert context.tasks_added, "Tasks were not created successfully"
+    assert len(context.todo_list.task_list) == 3, "Not all tasks were added"
 
-@then('the task should appear in my to-do list')
-def availability_check(context):
-    found = False
+@then('the tasks should appear in my to-do list')
+def verify_tasks_in_list(context):
+    expected_tasks = {
+        "Make homework": {
+            "description": "You need make your python homework",
+            "deadline": "20.02.2025",
+            "priority": "Medium"
+        },
+        "Buy some products": {
+            "description": "Buy milk, cheese and beef",
+            "deadline": "17.02.2025",
+            "priority": "Low"
+        },
+        "Call mom": {
+            "description": "Call mom and wish happy birthday",
+            "deadline": "18.02.2025",
+            "priority": "High"
+        }
+    }
+
+    found_tasks = {task: False for task in expected_tasks}
+
     for task in context.todo_list.task_list:
-        if task.task_title == "Make homework":
-            found = True
-            assert task.task_text == "You need make your python homework"
-            assert task.deadline == "20.02.2025"
-            assert task.priority == "Medium"
-            break
-    assert found, "Task was not found in the list"
+        if task.task_title in expected_tasks:
+            found_tasks[task.task_title] = True
+            expected = expected_tasks[task.task_title]
+            assert task.task_text == expected["description"], f"Wrong description for {task.task_title}"
+            assert task.deadline == expected["deadline"], f"Wrong deadline for {task.task_title}"
+            assert task.priority == expected["priority"], f"Wrong priority for {task.task_title}"
+
+    for task_title, found in found_tasks.items():
+        assert found, f"Task '{task_title}' was not found in the list"
--- features/to_do_list.feature
+++ features/to_do_list.feature
@@ -4,9 +4,12 @@ Feature: To do list functionality
   So that I can organize my work effectively
 
   Scenario: Successful task creation and adding it to the list
-    Given I want to create a task with the following details:
-      | title         | description                       | deadline    | priority |
-      | Make homework | You need make your python homework| 20.02.2025  | Medium   |
-    When I add the task "Make homework" to the list
-    Then the task should be successfully created
-    And the task should appear in my to-do list
+    Given I want to create a tasks with the following details:
+      | title             | description                       | deadline    | priority |
+      | Make homework     | You need make your python homework| 20.02.2025  | Medium   |
+      | Buy some products | Buy milk, cheese and beef         | 17.02.2025  | Low      |
+      | Call mom          | Call mom and wish happy birthday  | 18.02.2025  | High     |
+
+    When I add the tasks "Make homework", "Buy some products" and "Call mom" to the list
+    Then the tasks should be successfully created
+    And the tasks should appear in my to-do list
